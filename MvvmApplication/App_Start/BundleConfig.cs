9b355f37fea79f598401d49b10ee98845e0a1840
Luke <lukezeng@live.com>
2015-03-26 22:06:04 -0400

    added bootstrap to _Layout.cshtml

--- MvvmApplication/App_Start/BundleConfig.cs
+++ MvvmApplication/App_Start/BundleConfig.cs
@@ -13,6 +13,9 @@ namespace MvvmApplication
             
             bundles.Add(new ScriptBundle("~/bundles/angular").Include(
                         "~/Scripts/angular.js"));
+            
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                        "~/Scripts/bootstrap.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
                         "~/Scripts/jquery-ui-{version}.js"));
@@ -27,6 +30,8 @@ namespace MvvmApplication
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/bootstrap").Include("~/Content/bootstrap.css",
+                "~/Content/bootstrap-theme.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
