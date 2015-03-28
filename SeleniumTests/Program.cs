6e3fb6d71704704ed70394a16b645148e053ef6a
Luke <lukezeng@live.com>
2015-03-28 18:35:47 -0400

    addedt seleniumTest

---
+++ SeleniumTests/Program.cs
@@ -0,0 +1,14 @@
+﻿using OpenQA.Selenium.Firefox;
+
+namespace SeleniumTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("http://www.baidu.com");
+            driver.Close();
+        }
+    }
+}
