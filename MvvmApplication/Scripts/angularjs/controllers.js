58f3a16c5463df2bb3e6218d8c38d6441f2090a9
Luke <lukezeng@live.com>
2015-03-27 20:14:13 -0400

    updated isActive for navCtrl

--- MvvmApplication/Scripts/angularjs/controllers.js
+++ MvvmApplication/Scripts/angularjs/controllers.js
@@ -1,5 +1,12 @@
 var controllers = angular.module("controllers", []);
 
+controllers.controller("NavCtrl", ['$scope', "$location", function ($scope, $location) {
+    $scope.isActive = function (route) {
+        console.log("["+route + "] [" + $location.path()+"]");
+        return route === $location.path();
+    };
+}]);
+
 controllers.controller("HomeCtrl", ['$scope', function ($scope) {
     $scope.title = 'Hello HomeCtrl';
 }]);
